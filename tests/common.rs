f0ea0ec02ea1780521b10b770da5193a7104dbb2
EddyXorb <eddyxorb@gmail.com>
2026-03-13 23:02:21 +0100

    refactor: new helper for tests to print logging and format.

---
+++ tests/common.rs
@@ -0,0 +1,14 @@
+use std::sync::Once;
+
+static INIT: Once = Once::new();
+
+/// Initialize tracing for tests (called only once per test run)
+/// Put this at the beginning of the test you want to debug.
+pub fn init_tests() {
+    INIT.call_once(|| {
+        let _ = tracing_subscriber::fmt()
+            .with_test_writer()
+            .with_max_level(tracing::Level::DEBUG)
+            .try_init();
+    });
+}
