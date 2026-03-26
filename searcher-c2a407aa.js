5ed9d552df5eae51c81364d97325c07efa84433e
EddyXorb <EddyXorb@users.noreply.github.com>
2026-03-26 22:26:17 +0000

    deploy: 74a9c7db1cf106bfe88452b0e15837e78d459d81

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-34e43021.js',
+                path_to_root + 'searchindex-40322265.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
